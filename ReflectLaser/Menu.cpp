5fb9f40716b42615c18e41bbab8640e4e767a752
shp241 <1535193592@qq.com>
2019-07-01 18:34:41 +0800

    Create Menu.cpp

---
+++ ReflectLaser/Menu.cpp
@@ -0,0 +1,33 @@
+#include"menu.h"
+#include<easyx.h>
+#include<graphics.h>
+#include"button.h"
+#include"menu_operate.h"
+
+void menu() {
+	Picture p1;
+
+	p1.initgraph();
+
+	Button* BStart = new Button(new Point(, ), new Point(, ));
+	Button* BOption = new Button(new Point(, ), new Point(, ));
+	Button* BHelp = new Button(new Point(, ), new Point(, ));
+	Button* BExit = new Button(new Point(, ), new Point(, ));
+
+	for (;;) {
+		MOUSEMSG m;
+		m = GetMouseMsg();
+		if (BStart->if_click(m.x, m.y)) {
+			start();
+		}//鼠标点击开始游戏
+		if (BOption->if_click(m.x, m.y)) {
+			option();
+		}//选项
+		if (BHelp->if_click(m.x, m.y)) {
+			help();
+		}//帮助
+		if (BExit->if_click(m.x, m.y)) {
+			exit();
+		}//退出
+	}
+}
