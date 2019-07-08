282d6ce9042e2448a8c8286a9116ff02ce6ece10
shp241 <1535193592@qq.com>
2019-07-08 14:24:09 +0800

    Revert "Revert "add Items""
    This reverts commit fec14713f098f72ba75d589f5e2ff0dc6c58a9c5.

--- ReflectLaser/Block.h
+++ ReflectLaser/Block.h
@@ -1,22 +1,22 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include "Button.h"
 #include "RelativePoint.h"
 #include "System.h"
 using std::string;
+using std::to_string;
 
 class Block :public Button {//单一地图块类
 private:
 	RelativePoint* p;//地图块的相对坐标
 	bool empty;//ture则表示该地图块为空
-	bool moving;//ture则表示该地图块可移动
 public:
-	Block(RelativePoint* p, bool empty = true, bool moving = false);
+	Block(RelativePoint* p, bool empty = true);
 	Block(const Block& b);
 	RelativePoint* getPosition()const;
 	void setPosition(RelativePoint* p);
 	bool isEmpty()const;//判断该地图块是否为空
-	bool canMove()const;//判断该地图块是否能移动
 	virtual string getImage()const;//获取该地图块上道具的图片名
 	virtual void role();
 	virtual ~Block();
