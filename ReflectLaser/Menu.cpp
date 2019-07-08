6799da02852d1121569d82f18a30c7fc88fd3540
shp241 <1535193592@qq.com>
2019-07-08 14:32:08 +0800

    Update Menu.cpp

--- ReflectLaser/Menu.cpp
+++ ReflectLaser/Menu.cpp
@@ -1,5 +1,9 @@
 #include "Menu.h"
 
+#include<windows.h>
+#include<stdio.h>
+#pragma comment(lib,"WinMM.Lib")
+
 //MainMenu类
 MainMenu::MainMenu(Point* p0, Point* p1) :Button(p0, p1) {}
 
@@ -23,7 +27,7 @@ Start::Start(Point* p0, Point* p1) :Button(p0, p1) {}
 void Start::role() {
 	System::clear();//进入该函数后，将容器中已有的按钮删除
 	Picture::putPicture("Menu\\ChapterChoose");//绘制关卡选择界面的背景
-	Button* BChapter[12];//实例化五个关卡选择按钮
+	Button* BChapter[12];//实例化十二个关卡选择按钮
 	Button* BMenu = new MainMenu(new Point(0, 0), new Point(150, 100));
 	System::add(BMenu);
 	int ChapterY[3] = { 160,290,430 };
@@ -46,15 +50,28 @@ Option::Option(Point* p0, Point* p1) :Button(p0, p1) {}
 
 void Option::role() {
 	System::clear();
-	!!//待补充
+	Button* BMusic = new Button(new Point(450, 100), new Point(450 + 250, 100 + 80));
+	Button* BMenu = new MainMenu(new Point(450, 480), new Point(450 + 250, 480 + 80));
+	System::add(BMenu);
+	System::add(BMusic);
+}
+//Music按钮
+Music::Music(Point* p0, Point* p1) :Button(p0, p1) {}
+
+void Music::role() {
+
+  PlaySound("Music\\MenuMusic.wav", NULL, SND_FILENAME | SND_ASYNC);
+
 }
 
 //Help类
 Help::Help(Point* p0, Point* p1) : Button(p0, p1) {}
 
 void Help::role() {
-	System::clear();
-	!!//待补充
+	System::clear();//进入该函数后，将容器中已有的按钮删除
+	Picture::putPicture("Menu\\Help");//绘制关卡选择界面的背景
+	Button* BMenu = new MainMenu(new Point(450,480), new Point(450+250,480+80));
+	System::add(BMenu);
 }
 
 //Exit类
@@ -73,7 +90,7 @@ Chapter::Chapter(Point* p0, Point* p1, int chapters) :Button(p0, p1) {
 void Chapter::role() {
 	System::clear();//进入该函数后，将容器中已有的按钮删除
 	Picture::putPicture("Menu\\PlayingWindow");//绘制关卡界面的背景
-	System::game = new Level("Level_" + to_string(chapters));
+	System::game = new Level("Level_" + chapters);
 	Button* BNext = new Chapter(new Point(640, 400), new Point(640 + 120, 400 + 40), (chapters + 1) % 12);
 	Button* BBack = new Start(new Point(640, 460), new Point(640 + 120, 460 + 40));
 	Button* BAntiClocked = new Rotate(new Point(640, 320), new Point(640 + 40, 320 + 40), false);
